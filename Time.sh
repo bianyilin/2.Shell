4181336b24246c492ff83f21ea12122955e629da
bianyilin <49523527+bianyilin@users.noreply.github.com>
2019-05-28 21:01:26 +0800

    Add files via upload

---
+++ Time.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+TimeBegin=`date +%s`
+RestBegin=0
+
+
+function CheckMin() {
+    TimeNow=`date +%s`
+    TimeBetween=$[ (${TimeNow} - ${TimeBegin}) / 60 ]
+    if [[ ${TimeBetween} -ge 45 ]]; then
+        return 1
+    fi
+    TimeBetween=$[ (${TimeNow} - ${TimeBegin}) / 60 ]
+    if [[ ${TimeBetween} -ge 50 ]]; then
+        return 1
+    fi
+    return 0
+}
+
+
+function CheckZero() {
+    Mini=`date +%M`
+    if [[ ${Mini} == "00" ]]; then
+        return 1
+    fi
+    return 0
+}
+
+while [[ true ]]; do
+    sleep 40
+    CheckMin
+    if [[ $? -eq 1 ]]; then
+        echo "45"
+    fi
+    RestBegin=`date +%s`
+    RestTime
+    if [[ $? -eq 1 ]]; then
+        echo "Rest Time out"
+    fi
+    TimeBegin=`date +%s`
+    CheckZero
+    if [[ $? -eq 1 ]]; then
+        echo "00"
+    fi
+done
