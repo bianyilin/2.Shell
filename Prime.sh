b2ba36b2f56bb32a2889cc218f0c6f4e1b54dd4d
bianyilin <49523527+bianyilin@users.noreply.github.com>
2019-05-28 21:02:20 +0800

    Add files via upload

--- Prime.sh
+++ Prime.sh
@@ -1,21 +1,34 @@
-#!/bin/bash
-for (( i = 2; i <= 1000; i++));do
-    if [[ ${prime[$i]} -eq 0 ]]; then
+#!/bin/bash 
+#线性筛
+
+declare -a prime
+sum=0
+function init() {
+    for i in `seq 1 1000`;do
+        eval $1[$i]=0
+    done
+}
+
+init prime
+
+for ((i=2; i<1000; i++));do
+    if [[ ${prime[$i]} -eq 0 ]];then
         prime[0]=$[ ${prime[0]}+1 ]
-        prime[${prime[0]}]=$i
+        prime[ ${prime[0]} ]=$i
+        sum=$[ $i+$sum ]
     fi
-    for(( j = 1; j <= ${prime[0]}; j++ ));do
-        if [[ $[ ${prime[$j]} * ${i}] -gt 1000 ]]; then
+    for ((j=1; j<=prime[0]; j++));do
+        if [[ $[ ${prime[$j]} * $i ] -ge 1000 ]];then    #超出界限跳出
             break
         fi
-        prime[$[ ${prime[$j]}*$i ]]=1
-        if [[ $[ ${i}%${prime[$j]} ] -eq 0 ]]; then
-            break
+            prime[ ${prime[$j]}*$i ]=1                      
+        if [[  $[ $i % ${prime[$j]} ]  -eq 0 ]];then
+            break;
         fi
+
     done
 done
-
-for (( i = 1; i <= prime[0]; i++ )); 
-do
+for i in `seq 1 ${prime[0]}`;do
     echo ${prime[$i]}
 done
+echo $sum
